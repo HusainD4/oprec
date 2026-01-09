88474cb87252679adfe68cc05a1563617e42fac3
Husain Mulyansyah <145633025+HusainD4@users.noreply.github.com>
2026-01-09 20:07:57 +0700

    update maintenance

--- data.ts
+++ data.ts
@@ -1,57 +1,57 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyC2yQsDR6J3dzkdTJAZDtA6BKTiKvqqhSI",
-  authDomain: "myapp-c1981.firebaseapp.com",
-  databaseURL: "https://myapp-c1981-default-rtdb.firebaseio.com",
-  projectId: "myapp-c1981",
-  storageBucket: "myapp-c1981.firebasestorage.app",
-  messagingSenderId: "721330953305",
-  appId: "1:721330953305:web:26d329ff6ad1611918a548",
-  measurementId: "G-7BW3KJLJ66"
-};
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyC2yQsDR6J3dzkdTJAZDtA6BKTiKvqqhSI",
+//   authDomain: "myapp-c1981.firebaseapp.com",
+//   databaseURL: "https://myapp-c1981-default-rtdb.firebaseio.com",
+//   projectId: "myapp-c1981",
+//   storageBucket: "myapp-c1981.firebasestorage.app",
+//   messagingSenderId: "721330953305",
+//   appId: "1:721330953305:web:26d329ff6ad1611918a548",
+//   measurementId: "G-7BW3KJLJ66"
+// };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
 
 
-    <script>
-  // Disable F12
-  document.addEventListener('keydown', function (e) {
-    if (e.key === "F12") {
-      e.preventDefault();
-      return false;
-    }
+//     <script>
+//   // Disable F12
+//   document.addEventListener('keydown', function (e) {
+//     if (e.key === "F12") {
+//       e.preventDefault();
+//       return false;
+//     }
 
-    // Disable Ctrl+Shift+I
-    if (e.ctrlKey && e.shiftKey && e.key === "I") {
-      e.preventDefault();
-      return false;
-    }
+//     // Disable Ctrl+Shift+I
+//     if (e.ctrlKey && e.shiftKey && e.key === "I") {
+//       e.preventDefault();
+//       return false;
+//     }
 
-    // Disable Ctrl+Shift+J
-    if (e.ctrlKey && e.shiftKey && e.key === "J") {
-      e.preventDefault();
-      return false;
-    }
+//     // Disable Ctrl+Shift+J
+//     if (e.ctrlKey && e.shiftKey && e.key === "J") {
+//       e.preventDefault();
+//       return false;
+//     }
 
-    // Disable Ctrl+Shift+C
-    if (e.ctrlKey && e.shiftKey && e.key === "C") {
-      e.preventDefault();
-      return false;
-    }
+//     // Disable Ctrl+Shift+C
+//     if (e.ctrlKey && e.shiftKey && e.key === "C") {
+//       e.preventDefault();
+//       return false;
+//     }
 
-    // Disable Ctrl+U (View Source)
-    if (e.ctrlKey && e.key === "u") {
-      e.preventDefault();
-      return false;
-    }
-  });
-</script>
+//     // Disable Ctrl+U (View Source)
+//     if (e.ctrlKey && e.key === "u") {
+//       e.preventDefault();
+//       return false;
+//     }
+//   });
+// </script>
